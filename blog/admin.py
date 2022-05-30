1fa832ca131b69096fa20e5b8adbf05bf82aaea9
vianney bailleux <vianneyba@free.fr>
2022-05-30 09:40:44 +0200

    create models blog + admin

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from blog.models import Article, Category, Tag
+
+class BlogAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_display = ('title', 'author')
+    list_filter = ('published',)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Article, BlogAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
---
+++ blog/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField(default='')
+	slug = models.SlugField()
+	created_at = models.DateTimeField(default=timezone.now)
+	published = models.BooleanField(default=False)
+	update_date = models.DateTimeField(blank=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	like_count = models.IntegerField(default=0)
+	dislike_count = models.IntegerField(default=0)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		verbose_name = 'Gestion de l\'article'
+		verbose_name_plural = 'Gestion des articles'
