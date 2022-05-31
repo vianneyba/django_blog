eaf8879df00ebfad88d53cd8b6e542a03c40a0d8
vianney bailleux <vianneyba@free.fr>
2022-05-31 12:40:02 +0200

    change menu bar

--- vianneyba/context_processors.py
+++ vianneyba/context_processors.py
@@ -4,4 +4,6 @@ def admin_media(request):
     return {
         'WITH_COMMENT': settings.WITH_COMMENT,
         'WITH_REGISTRATION': settings.WITH_REGISTRATION,
+        'TITLE_SITE': settings.TITLE_SITE,
+        'CATEGORIES': [('jeux-video', 'Jeux Vidéo')],
     }
