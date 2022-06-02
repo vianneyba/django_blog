8512011fce539f6104d2bec0ca3e16df00feea3f
vianney bailleux <vianneyba@free.fr>
2022-06-02 11:59:54 +0200

    fix login template

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class loginForm(forms.Form):
-	username= forms.CharField(label='Your name', max_length=100)
-	password= forms.CharField(label='password', max_length=100)
+	username = forms.CharField(label='Username: ', max_length=100)
+	password = forms.CharField(label='Password: ', max_length=100)
